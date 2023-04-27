ff7610e2b777dcf6d1c1df8a30dff7365024489c
karna <harishgontu@gmail.com>
2023-04-26 19:31:50 -0500

    beachline

--- pages/voronoi/util.js
+++ pages/voronoi/util.js
@@ -40,21 +40,48 @@ function drawSweepLine() {
 }
 
 function drawBeachLine() {
-    for (var i = 0; i < beachLine.length; i++) {
-        var point_i = beachLine[i];
-        var pre_x = 0.0;
-        var pre_y = 0.0;
-        for (var x_i = 0; x_i < width - 1; x_i++) {
-            var t1 = (x_i * x_i) - (2 * x_i * point_i.x) + (point_i.x * point_i.x) + (point_i.y * point_i.y) - (sweepLineY * sweepLineY);
+    // for (var i = 0; i < beachLine.length; i++) {
+    //     var point_i = beachLine[i];
+    //     var pre_x = 0.0;
+    //     var pre_y = 0.0;
+    //     for (var x_i = 0; x_i < width - 1; x_i++) {
+    //         var t1 = (x_i * x_i) - (2 * x_i * point_i.x) + (point_i.x * point_i.x) + (point_i.y * point_i.y) - (sweepLineY * sweepLineY);
+    //         var t2 = 2 * (point_i.y - sweepLineY);
+    //         var y = t1 / t2;
+    //         strokeWeight(1);
+    //         stroke(0, 0, 255);
+    //         line(pre_x, pre_y, x_i, y);
+    //         pre_x = x_i;
+    //         pre_y = y;
+    //     }
+    // }
+
+    for (var x_i = 0; x_i < width - 1; x_i++) {
+         var x1 = x_i;
+         var x2 = x_i + 1;
+         var y1 = 0.0;
+         var y2 = 0.0;
+
+        for (var i = 0; i < beachLine.length; i++) {
+            var point_i = beachLine[i];
+
+            var t1 = (x1 * x1) - (2 * x1 * point_i.x) + (point_i.x * point_i.x) + (point_i.y * point_i.y) - (sweepLineY * sweepLineY);
             var t2 = 2 * (point_i.y - sweepLineY);
-            var y = t1 / t2;
-            strokeWeight(1);
-            stroke(0, 0, 255);
-            line(pre_x, pre_y, x_i, y);
-            pre_x = x_i;
-            pre_y = y;
+            
+            y1 = max(y1,t1 / t2);
+            
+            t1 = (x2 * x2) - (2 * x2 * point_i.x) + (point_i.x * point_i.x) + (point_i.y * point_i.y) - (sweepLineY * sweepLineY);
+            t2 = 2 * (point_i.y - sweepLineY);
+            y2 = max(y2,t1 / t2);
+            
+            
         }
+        strokeWeight(1);
+        stroke(0, 255, 0);
+        line(x1, y1, x2, y2);
     }
+
+
 }
 
 function updateSweepLineAndDoNextEventMaybe() {
